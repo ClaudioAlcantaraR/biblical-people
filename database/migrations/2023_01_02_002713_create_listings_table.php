57c99a18e3e09e29618858f83556711eaf0d24eb
Claudio <claudio.dev29@gmail.com>
2023-01-02 01:49:40 +0100

    installing darkmode, seed, mrc

---
+++ database/migrations/2023_01_02_002713_create_listings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('birthplace');
+            $table->date('birthday');
+            $table->string('deathplace');
+            $table->date('deathdate');
+            $table->longText('description');
+            $table->string('tags');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listings');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +16,62 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::factory(5)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Listing::create([
+            'name' => 'Jose', 
+            'birthplace' => 'Juda',
+            'birthday' => '2007-12-31',
+            'deathplace' => 'Jerusalen',
+            'deathdate' => '2007-12-31',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?',
+            'tags' => 'tag1, tag2, tag3'
+        ]);
+
+        Listing::create([
+            'name' => 'Maria', 
+            'birthplace' => 'Santiago',
+            'birthday' => '2007-12-31',
+            'deathplace' => 'Grecia',
+            'deathdate' => '2007-12-31',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?',
+            'tags' => 'tag1, tag2, tag3'
+        ]);
+
+        Listing::create([
+            'name' => 'Pepe', 
+            'birthplace' => 'San cristobal',
+            'birthday' => '2007-12-31',
+            'deathplace' => 'Grecia',
+            'deathdate' => '2007-12-31',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?',
+            'tags' => 'tag1, tag2, tag3'
+        ]);
+
+        Listing::create([
+            'name' => 'Carlos', 
+            'birthplace' => 'Italia',
+            'birthday' => '2007-12-31',
+            'deathplace' => 'Grecia',
+            'deathdate' => '2007-12-31',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?',
+            'tags' => 'tag1, tag2, tag3'
+        ]);
+        
+        Listing::create([
+            'name' => 'Ruth', 
+            'birthplace' => 'España',
+            'birthday' => '2007-12-31',
+            'deathplace' => 'Grecia',
+            'deathdate' => '2007-12-31',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?',
+            'tags' => 'tag1, tag2, tag3'
+        ]);
     }
 }
