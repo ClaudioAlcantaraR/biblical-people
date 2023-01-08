4f3a145d1b87cb3562e73e3283ef8e7493ba2613
Claudio <claudio.dev29@gmail.com>
2023-01-08 17:38:17 +0100

    mejorando vista del single listing

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -11,23 +11,28 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 px-3">
-           <div class="w-full max-w-3xl mx-auto">
-            {{-- Nombre --}}
-            <h1 class="mb-2 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">{{ $listing->name }}</h1>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 px-3 flex justify-between">
+            <div class="w-full max-w-3xl">
+                {{-- Nombre --}}
+                <h1
+                    class="mb-2 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
+                    {{ $listing->name }}</h1>
                 {{-- Tags --}}
                 <div>
                     <x-listing-tags :tagsCsv="$listing->tags" />
                 </div>
                 {{-- Lectura TODO: Hard Code--}}
                 <div>
-                   <span class="text-gray-900 font-semibold text-sm">15 min de lectura</span>
+                    <span class="text-gray-900 font-semibold text-sm">15 min de lectura</span>
                 </div>
                 {{-- Content --}}
                 <div class="mt-8">
                     <p class="text-gray-900 dark:text-gray-300 lg:text-lg text-base">{{ $listing->content }}</p>
                 </div>
-           </div>
+            </div>
+            <aside class="hidden lg:block lg:w-80" aria-label="Sidebar">
+                <x-quick-bio :listing="$listing" />
+            </aside>
         </div>
     </div>
 </x-app-layout>
